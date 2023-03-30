5a6f594afcd4deffb13eed1851df6ff8ce58bbfa
rmakarov <rmakarov@wiley.com>
2023-03-30 12:53:29 +0300

    NOJIRA: Added routing.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home.jsx'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>My React App</h1>
-            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <Home />}/>
+                </Routes>
+            </BrowserRouter>
         );
     }
 }
